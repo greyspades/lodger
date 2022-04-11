a618813eca5081d7c8e87e630fa6561049076379
chidera <greyspades99@gmail.com>
2022-04-11 20:14:03 +0100

    completed expansion panel for notifications

--- lib/components/notifications.dart
+++ lib/components/notifications.dart
@@ -5,13 +5,19 @@ import 'package:lodger/components/avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:path_provider/path_provider.dart';
-
+import 'package:intl/intl.dart';
 
 class Item  {
-  bool expanded;
+  bool? expanded;
+  List? dataList;
   Map<String, dynamic>? data;
   DocumentSnapshot? info;
-  Item({this.data,this.info,this.snapshot,this.expanded=false});
+  Item({this.data,this.info,this.snapshot,this.expanded,this.dataList});
+
+void _setExpand(){
+  expanded=true;
+}
+
 
   AsyncSnapshot? snapshot;
 
@@ -36,55 +42,113 @@ class _NotificationState extends State<Notifications> {
   //bool raisedPanel=false;
   //double panelHeight=_expandableController.value ? 470 :100;
 
+List<Item> generateItems(int numberOfItems,  data) {
+
+  return List<Item>.generate(numberOfItems, (int index) {
+     Map<String, dynamic> messages={};
+    // Map<String, dynamic> messages=data.map((item){
+    //   return item as Map<String, dynamic>;
+    // });
+    data.forEach((item){messages['subject']=item['subject']; messages['sender']=item['sender'];});
+    // Map<String, dynamic> messages=data.map((item)=>{
+    //   'subject':item['subject'],
+    // });
+     //Map<String, dynamic> messages=Map.fromIterable(data,key: (item)=>item[])
+
+    return Item(
+      
+      data: messages,
+      dataList:data
+    );
+  });
+}
+
     
 
   Widget expansionPanel(AsyncSnapshot snapshot){
 
     return Stack(
       children:snapshot.data!.docs.map<Widget>((DocumentSnapshot document){
-         Map<String, dynamic> data=document.data() as Map<String, dynamic>;
-         Item first=new Item(data: data);
-            return ExpansionPanelList(
-      expansionCallback: (int index, bool isExpanded){
+         Map<String, dynamic> doc=document.data() as Map<String, dynamic>;
        
-        setState(() {
-          first.expanded=true;
-        });
-      },
-      children: first.data?['mail'].map<ExpansionPanel>((d){
-        //Map<String, dynamic> data=document.data() as Map<String, dynamic>;
-         //Item item=new Item(data: data);
-         Item mail=new Item(data: d);
-        return ExpansionPanel(
-          headerBuilder: (BuildContext context, bool isExpanded){
-            
-          return Container(
-                              child: ListTile(
-                      // onTap: (){
-                      //   print('tapped');
-                      //    setState(() {
-                      //      mail.expanded=true;
-                      //    });
-                      // },
-                      leading: Container(child: CircleAvatar(),),
-                      title: Text(mail.data?['subject']),
-                      tileColor:Color(0xffFBF0EA) ,
-                      
+           var items=doc['mail'].map((item){
+             //print(item);
+             Map<String, dynamic> message=item;
+            return Item(
+              data: item,
+              expanded: false
+            );
+            //return item;
+              }).toList();
+
+              return Container(
+                height: 300,
+                child: ListView(
+                children: doc['mail'].map<Widget>((d){
+                  //var time=DateTime.fromMillisecondsSinceEpoch(d['time'].toDate());
+
+                  var formatedTime=DateFormat.yMMMd().format(d['time'].toDate());
+                  //String priority=d['']
+
+                  return Container(
+                    color:Color(0xffFBF0EA),
+                      margin: EdgeInsets.only(left: 5,right: 5,bottom: 5,top: 5),
+                    //*expandable widget for notifications
+                    child: ExpandablePanel(
+                      collapsed: Container(
+                    color:Color(0xff9F3647) ,
+                  
                    
-                    )
-          );
-        }, body: Container(
-          child: Text(mail.data?['subject']),
-        ),
-        isExpanded: mail.expanded 
-        //isExpanded: true
-        );
-      }).toList()
-    );
+                  ), expanded: Container(
+
+                    color:Color(0xff9F3647),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+
+                       //*row for the sender details of the mail
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text(formatedTime,style: TextStyle(color: Colors.white),),
+                       Text('from: ${d['sender']}',style: TextStyle(color: Colors.white),),
+                       Text('priority: ${d['critical'] ? 'High' : 'Low'}',style: TextStyle(color: Colors.white),),
+                       ], 
+                       ),
+                     
+                        //* notification body
+                       Row(children: [
+                         Container(
+                           margin: EdgeInsets.only(top: 30,bottom: 5),
+                           child:  Text(d['item'],style: TextStyle(color: Colors.white),),
+                         )
+                       ],),
+
+                    ],)
+                    ),
+
+                    //*header for the expanson panel
+                    header: Container(
+                      
+                      margin: EdgeInsets.only(left: 5,right: 5,top:5,bottom: 5),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)),color: Color(0xffFBF0EA),),
+                      child: ListTile(title: Text(d['subject']),leading: CircleAvatar(),focusColor:Color(0xffFBF0EA),)
+                    ),
+                    ),
+                  );
+                }).toList(),
+              ),
+              );
+
+              
+          
+          
+          
          }).toList() ,
     );
 
-    
+       
+      
   }
 
   
@@ -99,13 +163,7 @@ class _NotificationState extends State<Notifications> {
     double panelHeight=_expandableController.value ? 470 :100;
     //print(_expandableController.value);
 
-    if(_expandableController.value==true){
-      print('expanded');
-    }
-
-    else if(_expandableController.value==false){
-      print('not expanded');
-    }
+    
 
     return Container(
       //height: panelHeight,
@@ -127,154 +185,12 @@ class _NotificationState extends State<Notifications> {
         ),
       );
 
-    // return ExpansionPanelList(
-    //   children: snapshot.data!.docs.map((DocumentSnapshot document){
-    //      //Map<String, dynamic> data=document.data() as Map<String, dynamic>;
-         
-    //     return ExpansionPanel(headerBuilder: (BuildContext context, bool isExpanded){
-    //       return Container();
-    //     }, body: Container());
-    //   }).toList()
-
-    // );
+    ;
 
 
 
 
-    //     return ListView(
-    //       children: snapshot.data!.docs.map((DocumentSnapshot document){
-    //         Map<String, dynamic> data=document.data() as Map<String, dynamic>;
-           
-    //  return Stack(
-    //       children: data['mail'].map<Widget>((d){
-    //             return Container(
-                  
-    //             color: Color(0xff9F3647),
-    //               margin: EdgeInsets.all(3),
-    //               child: !d['read'] ? ExpandableNotifier(
-
-    //                 controller: _expandableController,
-    //                 child: ExpandablePanel(
-    //                 // builder: ((context, collapsed, expanded,) {
-    //                 //   return Container(
-    //                 //     // height: 200,
-    //                 //     // color: Colors.white,
-    //                 //     child: ExpandablePanel(collapsed: collapsed,expanded:expanded,)
-    //                 //   );
-                      
-    //                 // }),
-    //                 controller: _expandableController,
-                    
-
-    //                 header: Container(
-                      
-    //                   color: Color(0xffFBF0EA) ,
-
-                      
-    //                   child: ListTile(
-    //                     onTap: (){
-    //                      // _expandableController.expanded;
-    //                       setState(() {
-    //                        //_expandableController.expanded=true;
-    //                        expand=true;
-    //                       });
-                          
-    //                     print(expand);
-    //                      },
-    //                   leading: Container(child: CircleAvatar(),),
-    //                   title: Text(d['subject']),
-    //                   tileColor:Color(0xffFBF0EA) ,
-                   
-    //                 ),
-                    
-    //                 ),
-    //                 expanded: Container(
-    //                   padding: EdgeInsets.only(left: 10,right: 10,top: 5),
-    //                   height: 130,
-
-    //                   decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(3),bottomLeft: Radius.circular(3),bottomRight: Radius.circular(3))),
-    //                   child: Column(
-    //                     crossAxisAlignment: CrossAxisAlignment.start,
-    //                     children: [
-    //                         Row(
-    //                           mainAxisAlignment: MainAxisAlignment.start,
-    //                           children: [
-    //                           Text("From",style:TextStyle(color: Colors.white)),
-
-    //                           //sender of mail
-
-    //                           Container(
-    //                             margin: EdgeInsets.only(left: 5,right: 25),
-    //                             child: Chip(
-                                  
-    //                               //backgroundColor:Color(0xffFBF0EA),
-    //                               label: Text(d['sender']),
-    //                              ),
-    //                           ),
-
-    //                           //urgency
-    //                           Row(children: [
-    //                             Text('Priority',style: TextStyle(color: Colors.white),),
-    //                             Container(
-    //                               margin: EdgeInsets.only(left: 5),
-    //                               child: Chip(label: Text(d['critical'] ? 'Major':'Minor',style: TextStyle(),)),
-    //                             )
-    //                             //Icon(icon:d['critical'] ? :Icons.)
-    //                           ],),
-
-                             
-                             
-    //                         ],
-    //                         ),
-                            
-    //                        Container(
-    //                          margin: EdgeInsets.only(top: 23),
-    //                          child:  Text(d['item'],style: TextStyle(color: Colors.white),),
-    //                        ),
-
-    //                        //button to mark as read
-    //                       Container(
-    //                         margin: EdgeInsets.only(top:20),
-    //                         child: Row(
-    //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                         children: [
-    //                         Text(d['time'].toDate().toString(),style: TextStyle(color: Colors.white),),
-
-
-    //                         //inner row to mark as read
-    //                         Row(children: [
-    //                           Text('mark read',style: TextStyle(color: Colors.white),),
-    //                           Checkbox(value: d['read'], onChanged: (bool? changed){
-                                
-    //                         },)
-    //                         ],)
-    //                       ],),
-    //                       ),
-
-    //                       //bottom card menu
-                          
-                        
-    //                   ]),
-                      
-    //                 ),
-
-    //                 collapsed: Container(
-    //                   // height: 150,
-    //                   // width: 360,
-    //                   // color: Colors.blue,
-    //                   // child: Text(d['subject']),
-    //                 ),
-    //               ))
-    //               :
-    //               Container(),
-    //             );
-    //           }).toList()
-    //   );
     
-
-
-    //         // );
-    //       }).toList(),
         //);
               },
               
